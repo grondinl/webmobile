e08ac12e43e84aa0d08981d611d50a1c12533b59
Laurent Grondin <laurent.grondin@ensimag.grenoble-inp.fr>
2017-05-03 20:19:13 +0200

    aafiche contact utilisateur

--- www/js/contact2.js
+++ www/js/contact2.js
@@ -6,7 +6,7 @@
 
 $(document).ready(function(){  
     function onDeviceReady () {             
-        getContactList();
+        getContactList()
         $("#changerPageMessage").on('click', function(){
             window.location='message.html';
         });
